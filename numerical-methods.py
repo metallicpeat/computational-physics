1ccb61675884d253401e9ce98d7ea948e781543f
metallicpeat <129335717+metallicpeat@users.noreply.github.com>
2026-04-09 12:37:50 -0700

    Update numerical-methods.py
    added Euler Method

--- numerical-methods.py
+++ numerical-methods.py
@@ -1,1 +1,12 @@
 import numpy as np
+
+def Euler(fprime, time_max,time_step):
+    time = np.arange(0,time_max,time_step)
+    f = []
+    t = 0
+    while t*time_step < time_max:
+        f_t1 = f[t] + fprime * time_step
+        f.append(f_t1)
+        t+=1
+    f_array = np.array(f)
+    return time, f_array
